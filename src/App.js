94d4bd2bab65239268f2d2d96a52e8699aa7579c
uangbesok <Straus@Marinas-MacBook-Pro.local>
2020-01-09 23:42:42 +0800

    Add interface with routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Clarifai from "clarifai";
+import Navigation from "./components/Navigation/Navigation";
+import SignInForm from "./components/SignInForm/SignInForm";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Particles from "react-particles-js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particleOptions = {
+  particles: {
+    number: {
+      value: 70,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
+const app = new Clarifai.App({
+  apiKey: "1ca0d57f5fd642e5a84c6bb4e76ea9a8"
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageURL: "",
+      box: {},
+      route: "signIn",
+      isSignedIn: false,
+    };
+  }
+
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    // Here we use pure DOM jQuery manipulation for now but i change it to React approach
+    // 'passing ref'???? later on!!!!
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  saveFaceBox = box => {
+    this.setState({ box });
+  };
+
+  onSubmit = () => {
+    this.setState(
+      (state, props) => ({ imageURL: state.input }),
+      () => {
+        app.models
+          .predict(Clarifai.FACE_DETECT_MODEL, this.state.imageURL)
+          .then(response =>
+            this.saveFaceBox(this.calculateFaceLocation(response))
+          )
+          .catch(err => console.log(err));
+      }
+    );
+  };
+
+  onRouteChange = route => {
+    if(route === 'signIn')
+      this.setState({isSignedIn : false});
+    else if(route === 'home')
+      this.setState({isSignedIn : true});
+
+    this.setState({ route });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {/* <Particles className="particles" params={particleOptions} /> */}
+        <Navigation onRouteChange={this.onRouteChange} isSignedIn={this.state.isSignedIn}/>
+        {this.state.route === "signIn" ? (
+          <SignInForm onRouteChange={this.onRouteChange} />
+        ) : this.state.route === "home" ? (
+          <>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onSubmit={this.onSubmit}
+            />
+            <FaceRecognition
+              imageURL={this.state.imageURL}
+              box={this.state.box}
+            />
+          </>
+        ) : (
+          <RegisterForm onRouteChange={this.onRouteChange} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ImageLinkForm.css"
+
+const ImageLinkForm = ({ onInputChange, onSubmit }) => {
+  return (
+    <div>
+      <p className="f3">This Brain Bla-Bla-Bla</p>
+      <div className="center">
+        <div className="form pa4 br3 shadow-5 center">
+          <input className="f4 pa2 w-70 center" type="text" onChange={onInputChange}/>
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-dark-pink" onClick={onSubmit}>
+            Detect
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageLinkForm;
