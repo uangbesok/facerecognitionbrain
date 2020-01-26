4da7ffd7411bb380f41016ae45a6d06cdc647609
uangbesok <45453585+uangbesok@users.noreply.github.com>
2020-01-26 15:34:23 +0800

    Updates

---
+++ precache-manifest.9a33c414ebbed1bace51897c9445f4a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2795f23445a77a0aba3f21b0ca23817",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "fddd7bd8b461be7328d0",
+    "url": "/facerecognitionbrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e3b38476e64fed476d82",
+    "url": "/facerecognitionbrain/static/css/main.bf21e134.chunk.css"
+  },
+  {
+    "revision": "fddd7bd8b461be7328d0",
+    "url": "/facerecognitionbrain/static/js/2.ab158c20.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/facerecognitionbrain/static/js/2.ab158c20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3b38476e64fed476d82",
+    "url": "/facerecognitionbrain/static/js/main.cd2f707d.chunk.js"
+  },
+  {
+    "revision": "466a2f4fa9e4937c0bc5",
+    "url": "/facerecognitionbrain/static/js/runtime-main.a824e37d.js"
+  }
+]);
